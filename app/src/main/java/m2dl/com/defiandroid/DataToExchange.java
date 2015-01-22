a7ab369c1e6bb56f600c852efd683047480bb490
Guinoutortue <guinou@msn.com>
2015-01-22 10:23:33 +0100

    Nul mais je sauvegarde

--- app/src/main/java/m2dl/com/defiandroid/DataToExchange.java
+++ app/src/main/java/m2dl/com/defiandroid/DataToExchange.java
@@ -4,4 +4,16 @@ package m2dl.com.defiandroid;
  * Created by Guillaume on 22/01/2015.
  */
 public class DataToExchange {
+    float x;
+    float y;
+    float angle;
+    float speed;
+
+    public DataToExchange(float x, float y, float angle, float speed) {
+        this.x=x;
+        this.y=y;
+        this.angle=angle;
+        this.speed=speed;
+    }
+
 }
--- app/src/main/java/m2dl/com/defiandroid/MainActivity.java
+++ app/src/main/java/m2dl/com/defiandroid/MainActivity.java
@@ -1,23 +1,17 @@
 package m2dl.com.defiandroid;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import java.util.Set;
-
-import android.os.Bundle;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
-import android.content.Context;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import java.util.Set;
 
 
 public class MainActivity extends Activity {
@@ -32,23 +26,24 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView tv = (TextView) findViewById(R.id.tv);
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
         if (mBluetoothAdapter == null) {
             // Le terminal ne possède pas le Bluetooth
             tv.setText("Pas de Bluetooth");
-        }
-        else
-        {
-            if (!mBluetoothAdapter.isEnabled()) {
+        } else {
+            /*if (!mBluetoothAdapter.isEnabled()) {
                 System.out.println("Enabling BTAdapter");
                 mBluetoothAdapter.enable();
             }
             System.out.println("Beginning pairing of devices");
-        	/*Set<BluetoothDevice> pairedDevices = blueAdapter.getBondedDevices();
+        	Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+            ListView listView = (ListView) findViewById(R.id.expandableListView);
         	for (int i=0 ; i< pairedDevices.toArray().length ; i++) {
-        		System.out.println(pairedDevices.toArray()[i].toString());
+                TextView temp = new TextView(this);
+                temp.setText((pairedDevices.toArray()[i]).toString());
+        		listView.addFooterView(temp);
         	}*/
 
             //On présume que les trucs ont été pairés avant
@@ -59,16 +54,21 @@ public class MainActivity extends Activity {
                 // Loop through paired devices
                 for (BluetoothDevice device : pairedDevices) {
                     //Affichage des devices pairés
-                    s+="NAME:"+device.getName() + "\n" + "adress:"+device.getAddress()+"\n";
-                    //Je cherche celui dont j'ai besoin (en fait les 2 tel avec lesquels j'ai testé
-                    if (device.getName().equals("HTC Dada")) {
-                        System.out.println("HTDADA FOUND");
-                        this.htcDada=device;
-                    }
-                    if (device.getName().equals("HTC Desire C")) {
-                        this.htcC=device;
-                        System.out.println("HTCC FOUND");
-                    }
+                    s += "NAME:" + device.getName() + "\n" + "adress:" + device.getAddress() + "\n";
+//                    //Je cherche celui dont j'ai besoin (en fait les 2 tel avec lesquels j'ai testé
+//                    if (device.getName().equals("HTC Dada")) {
+//                        System.out.println("HTDADA FOUND");
+//                        this.htcDada=device;
+//                    }
+//                    if (device.getName().equals("HTC Desire C")) {
+//                        this.htcC=device;
+//                        System.out.println("HTCC FOUND");
+//                    }
+                      if (device.getName().equals("Galaxy Nexus 2")) {
+                          clientBT(findViewById(R.id.tvt));
+                      } else {
+                          serverBT(findViewById(R.id.tvt));
+                      }
 
                 }
                 tv.setText(s);
@@ -80,7 +80,7 @@ public class MainActivity extends Activity {
 
     //Lancement du server
     public void serverBT(View view) {
-        Log.i("ServerBT","Starting server");
+        Log.i("ServerBT", "Starting server");
         BluetoothServer btserv = new BluetoothServer(mBluetoothAdapter);
         btserv.start();
     }
@@ -88,12 +88,9 @@ public class MainActivity extends Activity {
     //Lancement du client
     public void clientBT(View view) {
         Log.i("ClientBT", "Starting client");
-        if (this.htcDada == null)
-        {
+        if (this.htcDada == null) {
             Log.e("ClientBT", "Device is null");
-        }
-        else
-        {
+        } else {
             System.out.println(this.htcDada.toString());
         }
         BluetoothClient btclient = new BluetoothClient(this.htcDada, mBluetoothAdapter); //le client est HTCC
